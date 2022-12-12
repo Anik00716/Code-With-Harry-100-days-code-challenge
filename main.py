2041528db18a39c5a7ac2f3279ac117a5fdf37dc
Anik Das <>
2022-12-12 13:23:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+number_1 = int(input('Please, Enter the first number: '))
+number_2 = int(input('Please, Enter the second number: '))
+ 
+#  Addition
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+ 
+#  Subtraction
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+ 
+# Multiplication
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+ 
+# Division
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
